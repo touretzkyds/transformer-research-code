25623fdcd514f438459f00ad74514e44036b90f9
neelpawarcmu <neelpawar17@gmail.com>
2026-06-18 19:40:41 -0400

    fix tokenizer loading error

--- tokenization/utils.py
+++ tokenization/utils.py
@@ -7,17 +7,25 @@ from utils.config import Config
 def build_tokenizers(config: Config):
     language_pair = config.dataset.language_pair
     tokenizer_type = config.tokenizer.type
-    cache = config.tokenizer.cache  
-    cache_path = op.join(config.logging.artifacts_dir, 
-                         config.logging.subdirs.tokenizers, 
-                         f"{tokenizer_type}_tokenizers.pt")
-    if (cache and op.exists(cache_path)):
-        tokenizer_src, tokenizer_tgt = torch.load(cache_path)
-        print(f"Loaded saved tokenizers from {cache_path}")
+    cache = config.tokenizer.cache
+
+    if cache:
+        cache_path = op.join(
+            config.logging.artifacts_dir,
+            config.logging.subdirs.tokenizers,
+            f"{tokenizer_type}_tokenizers.pt",
+        )
+        if op.exists(cache_path):
+            tokenizer_src, tokenizer_tgt = torch.load(cache_path)
+            print(f"Loaded saved tokenizers from {cache_path}")
+        else:
+            tokenizer_src, tokenizer_tgt = build_bert_tokenizers(language_pair)
+            os.makedirs(op.dirname(cache_path), exist_ok=True)
+            torch.save((tokenizer_src, tokenizer_tgt), cache_path)
+            print(f"Saved tokenizers to {cache_path}")
     else:
         tokenizer_src, tokenizer_tgt = build_bert_tokenizers(language_pair)
-        torch.save((tokenizer_src, tokenizer_tgt), cache_path)
-        print(f"Saved tokenizers to {cache_path}")
+
     print(f"Src vocab size: {len(tokenizer_src.vocab)}")
     print(f"Tgt vocab size: {len(tokenizer_tgt.vocab)}")
     return tokenizer_src, tokenizer_tgt
