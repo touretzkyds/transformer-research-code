d95ed505871e31516d840880c0aa3c003144185c
Dave <dst@cs.cmu.edu>
2026-08-20 19:19:59 -0400

    Rename a_2 and b_2 to scale and bias in LayerNorm

--- model/full_model.py
+++ model/full_model.py
@@ -197,14 +197,14 @@ class LayerNorm(nn.Module):
 
     def __init__(self, d_model, eps=1e-6):
         super(LayerNorm, self).__init__()
-        self.a_2 = nn.Parameter(torch.ones(d_model))
-        self.b_2 = nn.Parameter(torch.zeros(d_model))
+        self.bias = nn.Parameter(torch.ones(d_model))
+        self.scale = nn.Parameter(torch.zeros(d_model))
         self.eps = eps
 
     def forward(self, x):
         mean = x.mean(-1, keepdim=True)
         std = x.std(-1, keepdim=True)
-        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+        return self.scale * (x - mean) / (std + self.eps) + self.bias
 
 
 class EncoderLayer(nn.Module):
